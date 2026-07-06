ac49f0bc74b6012116b1d320a13e87ff3a41c29d
Shrushranto Rajbongshi <110803379+Shrushranto@users.noreply.github.com>
2026-07-06 19:23:42 +0530

    Add: 4sum - submission-5

---
+++ Data Structures & Algorithms/4sum/submission-5.java
@@ -0,0 +1,44 @@
+class Solution {
+    private List<List<Integer>> res;
+    private List<Integer> quad;
+    public List<List<Integer>> fourSum(int[] nums, int target) {
+        Arrays.sort(nums);
+        res = new ArrayList<>();
+        quad = new ArrayList<>();
+        kSum(nums, 4, 0, target);
+        return res;
+    }
+    private void kSum(int[] nums, int k, int start, long target) {
+        if (k != 2) {
+            for (int i = start; i < nums.length - k + 1; i++) {
+                if (i > start && nums[i] == nums[i - 1])
+                    continue;
+                quad.add(nums[i]);
+                kSum(nums, k - 1, i + 1, target - nums[i]);
+                quad.remove(quad.size() - 1);
+            }
+            return;
+        }
+
+        int l = start;
+        int r = nums.length - 1;
+        while (l < r) {
+            long sum = (long)nums[l] + nums[r];
+            if (sum > target) {
+                r--;
+            } else if (sum < target) {
+                l++;
+            } else {
+                res.add(new ArrayList<>(quad));
+                res.get(res.size() - 1).add(nums[l]);
+                res.get(res.size() - 1).add(nums[r]);
+                l++;
+                r--;
+
+                while (l < r && nums[l] == nums[l - 1]) l++;
+                while (l < r && nums[r] == nums[r + 1]) r--;
+            }
+        }
+        return;
+    }
+}
